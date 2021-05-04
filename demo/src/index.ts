f7280b33258f91cf1c5f7ef3f381e9e5aea0aaa5
shoushangdetuolaji <tklijunhao@qq.com>
2021-05-04 21:06:13 +0800

    add some gamecontrol

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,8 +1,16 @@
 import "./style/index.less";
-import Food from './modules/Food';
-import ScorePanel from './modules/ScorePanel'
+// import Food from './modules/Food';
+// import ScorePanel from './modules/ScorePanel'
 
-const food = new Food();
-console.log(food.X,food.Y);
-food.change();
-console.log(food.X,food.Y)
+// const food = new Food();
+// console.log(food.X,food.Y);
+// food.change();
+// console.log(food.X,food.Y)
+
+import GameControl from './modules/GameControl';
+
+const gameControl = new GameControl();
+
+// setInterval(()=>{
+//   console.log(gameControl.direction);
+// },1000);
